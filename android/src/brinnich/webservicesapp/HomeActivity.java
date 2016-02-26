d0902cb57fee846a6780e8c9114039697d25a396
Selina Brinnich <sbrinnich@student.tgm.ac.at>
2016-02-26 09:32:43 +0100

    Added android app

---
+++ android/src/brinnich/webservicesapp/HomeActivity.java
@@ -0,0 +1,18 @@
+package brinnich.webservicesapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ *
+ * Home Screen Activity
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Displays Home Screen
+        setContentView(R.layout.home);
+    }
+
+}
